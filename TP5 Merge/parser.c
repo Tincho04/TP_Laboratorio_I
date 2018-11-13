c988fb48f6a72f1a6749845c77b207ce00f07bf7
MaCh <40794845+Tincho04@users.noreply.github.com>
2018-11-13 20:23:33 -0300

    Merge TP3 & TP4
    Combinacion del LinkedList del TP4 implementado en el TP3

---
+++ TP5 Merge/parser.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "string.h"
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param FILE* pFile
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+    Employee* pAux;
+    char auxId[50];
+    char auxNombre[50];
+    char auxHorasTrabajadas[50];
+    char auxSueldo[50];
+    int flag = 1;
+    int retorno = -1;
+
+    if(pFile!= NULL)
+    {
+        while(!feof(pFile))
+        {
+            if(flag)
+            {
+                fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
+                flag = 0;
+            }
+        fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
+        pAux = employee_newParametros(auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
+            if(pAux !=NULL)
+            {
+            ll_add(pArrayListEmployee,pAux);
+            retorno = 0;
+            }
+        }
+    }
+    return retorno;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param FILE* pFile*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+  int retorno = -1;
+    int i;
+    Employee* pEmpleado;
+    int auxId;
+    int lastId;
+    char auxNombre[128];
+    int auxHorasTrabajadas;
+    int auxSueldo;
+    if(pFile!=NULL)
+    {
+        retorno = 0;
+        while(!feof(pFile))
+        {
+            pEmpleado = employee_new();
+            fread(pEmpleado,sizeof(Employee),1,pFile);
+            employee_getId(pEmpleado,&auxId);
+            employee_getName(pEmpleado,auxNombre);
+            employee_getHours(pEmpleado,&auxHorasTrabajadas);
+            employee_getSalary(pEmpleado,&auxSueldo);
+            if( auxId >= 0 && strlen(auxNombre) > 0 &&
+                auxHorasTrabajadas > 0 && auxSueldo > 0)
+            {
+                ll_add(pArrayListEmployee, pEmpleado);
+                lastId = auxId;
+            }
+        }
+        for(i=0;i<=lastId;i++)
+        {
+            employee_getNextId();
+        }
+    }
+    return retorno;
+}
+
+
+/** \brief Parsea los datos de los empleados para guardar en data.csv (modo texto).
+ *
+ * \param FILE* pFile
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_SaveToText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    int retorno = -1;
+    Employee* auxEmployee;
+    int len;
+    int i;
+    int auxId;
+    char auxNombre[1000];
+    int auxHorasTrabajadas;
+    int auxSueldo;
+
+    if(pFile != NULL && pArrayListEmployee != NULL)
+    {
+        len = ll_len(pArrayListEmployee);
+
+        for(i=0;i<len;i++)
+        {
+            auxEmployee = ll_get(pArrayListEmployee,i);
+            employee_getAll(auxEmployee,auxNombre,&auxHorasTrabajadas,&auxSueldo,&auxId);
+            fprintf(pFile,"%d,%s,%d,%d\n",auxId,auxNombre,auxHorasTrabajadas,auxSueldo);
+            retorno = 0;
+        }
+    }
+    return retorno;
+}
+
+/** \brief Parsea los datos de los empleados para guardar en data.bin (modo binario).
+ *
+ * \param FILE* pFile
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+
+int parser_SaveToBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    int retorno = -1;
+    int i = 0;
+    int auxId;
+    char auxNombre[51];
+    int auxHorasTrabajadas;
+    int auxSueldo;
+    Employee* auxEmployee;
+
+ if(pFile != NULL)
+    {
+        retorno = 0;
+        for(i=0;i<ll_len(pArrayListEmployee);i++)
+        {
+            auxEmployee = ll_get(pArrayListEmployee,i);
+            employee_getId(auxEmployee,&auxId);
+            employee_getName(auxEmployee,auxNombre);
+            employee_getHours(auxEmployee,&auxHorasTrabajadas);
+            employee_getSalary(auxEmployee,&auxSueldo);
+            if( auxId > 0 && strlen(auxNombre) > 0 &&
+                auxHorasTrabajadas > 0 && auxSueldo > 0)
+            {
+                fwrite(auxEmployee,sizeof(Employee),1,pFile);
+            }
+        }
+    }
+    return retorno;
+}
